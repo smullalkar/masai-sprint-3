b0747d0f0813745ed019fc59a07b020446f4e3d9
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-03-08 15:57:53 +0530

    Sprint-3 Project

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+var categoryObj
+var res = new XMLHttpRequest()
+res.open('GET', 'https://www.themealdb.com/api/json/v1/1/categories.php')
+res.send();
+
+var catDiv = document.getElementById('categories')
+catDiv.setAttribute('class', 'container-fluid')
+
+var row = document.createElement('div')
+row.setAttribute('class', 'row')
+catDiv.appendChild(row)
+
+res.onload = function () {
+    if (res.status == 200) {
+        categoryObj = JSON.parse(res.response)
+        console.log(categoryObj)
+
+
+
+        for (var i = 0; i < categoryObj.categories.length; i++) {
+
+            var col = document.createElement('div')
+            col.setAttribute('class', 'col-3')
+            row.appendChild(col)
+
+            var meal = document.createElement("div")
+            meal.setAttribute('class', 'card p-3 my-2 shadow-lg border-danger')
+            col.appendChild(meal)
+
+            var img = document.createElement("img")
+            var url = categoryObj.categories[i].strCategoryThumb
+            img.setAttribute("src", url)
+            img.setAttribute("class", "img-fluid")
+            meal.appendChild(img)
+
+            var cardBody = document.createElement("div")
+            cardBody.setAttribute('class', 'card-body')
+            meal.appendChild(cardBody)
+
+            var cardTitle = document.createElement("h5")
+            cardTitle.setAttribute('class', 'card-title text-center')
+            cardTitle.textContent = categoryObj.categories[i].strCategory
+            cardBody.appendChild(cardTitle)
+        }
+    }
+    else {
+        console.log("Error Code is:" + res.status);
+    }
+}
+
 var form = document.querySelector("form")
 form.addEventListener('submit', submit)
 
@@ -75,6 +125,7 @@ var rec = function details(e) {
 
     var body = document.getElementById("box")
     body.style.display = "none"
+    catDiv.style.display = "none"
 
     var mainBody = document.getElementById("showRecipe")
     mainBody.setAttribute('class', 'container-fluid')
@@ -91,9 +142,6 @@ var rec = function details(e) {
     recipeHead.textContent = obj.meals[index].strMeal + " ( Category : " + obj.meals[index].strCategory + " )"
     recipeHead.setAttribute('class', 'text-center my-2 card-header')
     col1.appendChild(recipeHead)
-
-    // var hr1 = document.createElement('hr')
-    // recipeHead.appendChild(hr1)
 
     var para = document.createElement("p")
     para.setAttribute('class', 'col text-center')
@@ -126,24 +174,24 @@ var rec = function details(e) {
     col3.setAttribute('class', 'col text-right')
     row2.appendChild(col3)
 
-    for(key in obj.meals[index]){
-        if(key != "dateModified" && key !=="idMeal" && key !== "strArea" && key !== "strCategory" && key !== "strDrinkAlternate" && key !== "strInstructions" && key !== "strMeal" && key !== "strMealThumb" && key !== "strMeasure1" && key !== "strMeasure10" && key !== "strMeasure11" && key !== "strMeasure12" && key !== "strMeasure13" && key !== "strMeasure14" && key !== "strMeasure15" && key !== "strMeasure16" && key !== "strMeasure17" && key !== "strMeasure18" && key !== "strMeasure19" && key !== "strMeasure20" && key !== "strMeasure2" && key !== "strMeasure3" && key !== "strMeasure4" && key !== "strMeasure5" && key !== "strMeasure6" && key !== "strMeasure7" && key !== "strMeasure8" && key !== "strMeasure9" && key !== "strSource" && key !== "strTags" && key !== "strYoutube" && key !== null){
+    for (key in obj.meals[index]) {
+        if (key != "dateModified" && key !== "idMeal" && key !== "strArea" && key !== "strCategory" && key !== "strDrinkAlternate" && key !== "strInstructions" && key !== "strMeal" && key !== "strMealThumb" && key !== "strMeasure1" && key !== "strMeasure10" && key !== "strMeasure11" && key !== "strMeasure12" && key !== "strMeasure13" && key !== "strMeasure14" && key !== "strMeasure15" && key !== "strMeasure16" && key !== "strMeasure17" && key !== "strMeasure18" && key !== "strMeasure19" && key !== "strMeasure20" && key !== "strMeasure2" && key !== "strMeasure3" && key !== "strMeasure4" && key !== "strMeasure5" && key !== "strMeasure6" && key !== "strMeasure7" && key !== "strMeasure8" && key !== "strMeasure9" && key !== "strSource" && key !== "strTags" && key !== "strYoutube" && key !== null) {
             var li = document.createElement("h6")
             li.textContent = obj.meals[index][key]
             li.setAttribute('class', 'small p-1 text-right col')
             col2.appendChild(li)
         }
-        
+
     }
 
-    for(key in obj.meals[index]){
-        if(key === "strMeasure1" || key === "strMeasure10" || key === "strMeasure11" || key === "strMeasure12" || key === "strMeasure13" || key === "strMeasure14" || key === "strMeasure15" || key === "strMeasure16" || key === "strMeasure17" || key === "strMeasure18" || key === "strMeasure19" || key === "strMeasure20" || key === "strMeasure2" || key === "strMeasure3" || key === "strMeasure4" || key === "strMeasure5" || key === "strMeasure6" || key === "strMeasure7" || key === "strMeasure8" || key === "strMeasure9" && key !== null){
+    for (key in obj.meals[index]) {
+        if (key === "strMeasure1" || key === "strMeasure10" || key === "strMeasure11" || key === "strMeasure12" || key === "strMeasure13" || key === "strMeasure14" || key === "strMeasure15" || key === "strMeasure16" || key === "strMeasure17" || key === "strMeasure18" || key === "strMeasure19" || key === "strMeasure20" || key === "strMeasure2" || key === "strMeasure3" || key === "strMeasure4" || key === "strMeasure5" || key === "strMeasure6" || key === "strMeasure7" || key === "strMeasure8" || key === "strMeasure9" && key !== null) {
             var li1 = document.createElement("h6")
             li1.textContent = obj.meals[index][key]
             li1.setAttribute('class', 'small p-1 text-left col')
             col3.appendChild(li1)
         }
-        
+
     }
 
     var backButton = document.createElement("button")
@@ -153,6 +201,7 @@ var rec = function details(e) {
     backButton.addEventListener('click', mainPage)
     function mainPage() {
         body.style.display = "inline"
+        catDiv.style.display = "inline"
         mainBody.style.display = "none"
     }
 }
